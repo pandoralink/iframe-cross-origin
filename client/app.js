a4c4b9455b94f2786429f59b228fe70a8d2590a2
poplink <2219535001@qq.com>
2022-12-07 00:08:10 +0800

    fix: 修补 express 实例导出

--- client/app.js
+++ client/app.js
@@ -3,8 +3,6 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-var indexRouter = require("./routes/index");
-
 var app = express();
 
 app.use(logger("dev"));
@@ -13,4 +11,4 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
+module.exports = app;
